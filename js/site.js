e49d2b16f42d3835559d5e36fb70ce113057b2b2
eprzystal <eprzystal@hawk.iit.edu>
2021-04-08 15:05:44 -0500

    added scrolling to html and background color to social media icons

---
+++ js/site.js
@@ -0,0 +1,5 @@
+var scroll-images = document.get ElementbyId ('scroll-images');
+
+scroll-images.addEventListener('click' function(name){
+    scroll-images.target.classlist.toggle('image1');
+    })
